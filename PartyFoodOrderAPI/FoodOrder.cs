87a3db2c24e47ff0225f3570564e4e46add8f9c3
Marlon Jestädt <marlon2019@outlook.de>
2022-02-05 14:29:21 +0100

    Add FoodOrder

---
+++ PartyFoodOrderAPI/FoodOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace PartyFoodOrderAPI
+{
+    public class FoodOrder
+    {
+        public DateTime OrderTime { get; set; }
+
+        public string? OrderedProduct { get; set; }
+
+        public int Count { get; set; }
+
+        public string? ConsumerName { get; set; }
+
+    }
+}
+ 
