1f38c5f22630c394e672006dd4586b3365e196ab
Marlon Jestädt <marlon2019@outlook.de>
2022-02-05 14:38:20 +0100

    Update FoodOrderController

--- PartyFoodOrderAPI/Controllers/FoodOrderController.cs
+++ PartyFoodOrderAPI/Controllers/FoodOrderController.cs
@@ -1,83 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace PartyFoodOrderAPI.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class FoodOrderController : Controller
     {
-        // GET: FoodOrderController
-        public ActionResult Index()
+
+        private readonly ILogger<FoodOrderController> _logger;
+
+        public FoodOrderController(ILogger<FoodOrderController> logger)
         {
-            return View();
+            _logger = logger;
         }
 
-        // GET: FoodOrderController/Details/5
-        public ActionResult Details(int id)
+        [HttpGet(Name = "GetFoodOrder")]
+        public string Get()
         {
-            return View();
-        }
-
-        // GET: FoodOrderController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: FoodOrderController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: FoodOrderController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: FoodOrderController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: FoodOrderController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: FoodOrderController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            _logger.Log(LogLevel.Information, "Request with body: " + Request.Body);
+            Console.WriteLine("test");
+            return "Hi!";
         }
     }
 }
