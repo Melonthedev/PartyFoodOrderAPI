c9b0eab07fef7c138d9f5c5b567524124eb8f41b
Marlon Jestädt <marlon2019@outlook.de>
2022-04-15 15:44:06 +0200

    Add functionality for editing products

--- PartyFoodOrderAPI/wwwroot/editproduct/script.js
+++ PartyFoodOrderAPI/wwwroot/editproduct/script.js
@@ -21,13 +21,45 @@ window.onload = () => {
     });
 };
 
-const selproduct = document.getElementById('selproduct');
+const selectedproduct = document.getElementById('selproduct');
 const delprod = document.getElementById('delprod');
+const savebutton = document.getElementById('savebutton');
+const outofstockcheckbox = document.getElementById('outofstock');
+const category = document.getElementById('category');
+const idfield = document.getElementById('id');
+const namefield = document.getElementById('name');
+const changestockbutton = document.getElementById('changestockbutton');
+
+selectedproduct.onchange = () => {
+    fetch('/api/FoodStock/GetProduct?id=' + selectedproduct.value, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+    }).then(response => response.json()
+    ).then(data => {
+        idfield.value = data.id;
+        namefield.value = data.name;
+        outofstockcheckbox.checked = !data.isInStock;
+        category.value = data.category;
+    }).catch(error => {
+        console.log(error);
+    });
+
+    delprod.disabled = false;
+    savebutton.disabled = false;
+    outofstockcheckbox.disabled = false;
+    category.disabled = false;
+    namefield.disabled = false;
+    changestockbutton.disabled = false;
+    idfield.disabled = false;
+}
+
 
 delprod.onclick = () => {
-    console.log(selproduct.value);
-    fetch('/api/FoodStock/DeleteProduct?id=' + selproduct.value, {
-        method: 'DELETE',
+    fetch('/api/FoodStock/DeleteProduct?productId=' + selectedproduct.value, {
+        method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
@@ -47,6 +79,45 @@ delprod.onclick = () => {
         console.log(error);
     });
 };
+
+changestockbutton.onclick = () => {
+    if (changestockbutton.classList.contains("markasinstockbutton")) {
+        changestockbutton.classList.remove("markasinstockbutton");
+        changestockbutton.classList.add("markasoutofstockbutton");
+        changestockbutton.innerText = "Markieren als 'Auf Lager'";
+        changeStockStatus(true);
+    } else if (changestockbutton.classList.contains("markasoutofstockbutton")) {
+        changestockbutton.classList.remove("markasoutofstockbutton");
+        changestockbutton.classList.add("markasinstockbutton");
+        changestockbutton.innerText = "Markieren als 'Nicht auf Lager'";
+        changeStockStatus(false);
+    }
+}
+
+
+function changeStockStatus(instock) {
+    fetch('/api/FoodStock/SetProductInStock/' + instock + '?productId=' + selectedproduct.value, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }).then(data => {
+        if(data.status == 200) {
+            document.write(`
+            <html lang="en">
+                <head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="/uistyle.css"><link rel="shortcut icon" type="image/png" href="./media/burger.png"/><title>Produkt gelöscht</title></head>
+                <body><img onclick="document.location = '/orders'" src="/orders/media/orders.png" width="40px" height="40px" id="homeicon">
+                    <h1 style="text-decoration: underline; text-align: center; margin-top: 70px; margin-bottom: 60px;">Produkt erfolgreich als 'Nicht auf Lager' makiert</h1>
+                    <button class="btn" onclick="document.location='/orders'">Zurück</button></body>
+            </html>`);
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+
+}
 
 
 document.onkeyup = (event) => {
