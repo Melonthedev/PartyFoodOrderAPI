64dce0bb27fddad742ead64f09f4c21d3098d3a8
Marlon Jestädt <marlon2019@outlook.de>
2022-04-30 17:23:16 +0200

    Add logic to mark outofstockproducts

--- PartyFoodOrderAPI/wwwroot/menuoverview/script.js
+++ PartyFoodOrderAPI/wwwroot/menuoverview/script.js
@@ -20,9 +20,11 @@ window.onload = () => {
             entry.classList.add('menu-item');
             headding.innerText = products[i].name;
             description.innerText = products[i].description;
+            description.style.wordBreak = "break-word";
             image.src = products[i].imageUrl;
             image.alt = products[i].name;
             image.style.width = "200px";
+            image.style.marginTop = "15px";
             image.style.borderRadius = "10px";
             orderbutton.classList.add('btn')
             orderbutton.classList.add('menuoverviewbtn')
@@ -30,12 +32,35 @@ window.onload = () => {
             orderbutton.onclick = () => {
                 document.location = '/placeorder?type=' + products[i].category + '&product=' + products[i].id;
             }
-            entry.appendChild(headding);
+            if (!products[i].isInStock) {
+                const outofstockheadding = document.createElement('h3');
+                outofstockheadding.innerText = " Momentan nicht auf Lager";
+                entry.appendChild(headding);
+                entry.appendChild(outofstockheadding);
+                headding.style.textDecorationLine = "line-through";
+                orderbutton.disabled = true;
+                entry.style.boxShadow = "0px 0px 10px red";
+            } else {
+                entry.appendChild(headding);
+            }
+            
             if (products[i].imageUrl != null) {
                 entry.appendChild(image);
             }
             entry.appendChild(description);
             entry.appendChild(orderbutton);
+            document.getElementById('body').appendChild(entry);
+        }
+        if (products.length == 0) {
+            const entry = document.createElement('div');
+            const headding = document.createElement('h3');
+            const description = document.createElement('p');
+            entry.classList.add('menu-item');
+            headding.innerText = "Keine Produkte vorhanden";
+            description.innerText = "Die verfügbare Produkte wurden wahrscheinlich noch nicht hinzugefügt.\n Bitte versuchen Sie es später noch einmal.";
+            description.style.marginBottom = "10px";
+            entry.appendChild(headding);
+            entry.appendChild(description);
             document.getElementById('body').appendChild(entry);
         }
     }).catch(error => {
